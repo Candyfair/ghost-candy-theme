85e212eb7280657e9b43eb97d78b6bac2c23801f
Candy Fair <candice.bastien@gmail.com>
2022-09-20 18:12:57 +0200

    Add speech bubble image in the header

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
-    // Get the menu + background
-    const menuBg = document.querySelector('.menu-mobile');
-    const menu = document.querySelector(".menu-mobile__container");
-    
-    // Open mobile menu
-    const openMenu = () => {
-      menuBg.classList.add("visible");
-      menu.classList.add("visible");
-    }
-    document.querySelector(".menu-mobile-burger").addEventListener("click", openMenu);
-    
-    // Outside clic to close the menu
-    document.addEventListener('click', event => {
-      console.log('clic', event.target)
-      if (event.target === menuBg) {
-        menuBg.classList.remove("visible");
-        menu.classList.remove("visible");
-      }
-    })
+// Get the menu + background
+const menuBg = document.querySelector('.menu-mobile');
+const menu = document.querySelector(".menu-mobile__container");
+
+// Open mobile menu
+const openMenu = () => {
+  menuBg.classList.add("visible");
+  menu.classList.add("visible");
+}
+document.querySelector(".menu-mobile-burger").addEventListener("click", openMenu);
+
+// Outside clic to close the menu
+document.addEventListener('click', event => {
+  if (event.target === menuBg) {
+    menuBg.classList.remove("visible");
+    menu.classList.remove("visible");
+  }
+})
